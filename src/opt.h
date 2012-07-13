74d90aa4b4a9c0645d29f7e3194d4ba809a4d86d
Michael Rodriguez <adeo2232@gmail.com>
2012-07-13 17:46:42 -0400

    Externally link CorcOpt

--- src/opt.h
+++ src/opt.h
@@ -4,13 +4,13 @@
 #include <libcorc/ds/list.h>
 #include <libcorc/core/str.h>
 
-typedef struct CorcOpt
+#define CORCOPT_REQUIRED 0x00
+#define CORCOPT_OPTIONAL 0x01
+
+extern typedef struct CorcOpt
 {
     CorcList *args;
 } CorcOpt;
-
-#define CORCOPT_REQUIRED 0x00
-#define CORCOPT_OPTIONAL 0x01
 
 extern CorcOpt *corcopt_init(void);
 extern void corcopt_fini(CorcOpt *co);
